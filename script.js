505f3c2075835a99b40f6dd2a4a698368b54f2dc
Nezabravka <nezabravka@test.com>
2025-06-29 10:24:24 +0300

    Squash last 3 commits into one from Chore branch

--- script.js
+++ script.js
@@ -2,4 +2,6 @@
 document.getElementById('apply-btn').addEventListener('click', function() {
   var color = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = color;
+  //commit 1 Chore branch
+  //commit 3 Chore branch
 });
